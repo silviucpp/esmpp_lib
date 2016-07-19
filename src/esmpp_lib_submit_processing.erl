981030efb32e8f9f7b08513570cee38b814616b1
silviu <silviu.caragea@woowteam.com>
2016-07-19 13:16:04 +0300

    Improve accuracy for submit_timeout param

--- src/esmpp_lib_submit_processing.erl
+++ src/esmpp_lib_submit_processing.erl
@@ -7,7 +7,7 @@
 
 -define(SERVER, ?MODULE).
 -define(DEFAULT_TIMEOUT, 60).
--define(DEFAULT_CHECK_INTERVAL, 60000).
+-define(MIN_EXPIRE_ACCURACY_MS, 5000).
 
 -export([start_link/1, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([processing_submit/4, push_submit/3]).
@@ -19,7 +19,8 @@ start_link(State) ->
     submit_check,
     submit_timeout_ms,
     handler,
-    parent_pid
+    parent_pid,
+    timer_ref
 }).
 
 processing_submit(Pid, SeqNum, List, OperationHandler) ->
@@ -32,7 +33,6 @@ init(Opt) ->
     HandlerPid = esmpp_utils:lookup(handler_pid, Opt),
     ParentPid = esmpp_utils:lookup(parent_pid, Opt),
     SubmitTimeoutMs = get_timeout(submit_timeout, Opt)*1000,
-    erlang:send_after(?DEFAULT_CHECK_INTERVAL, self(), check_not_ack_submit),
     {ok, #state{submit_check = [], submit_timeout_ms = SubmitTimeoutMs, handler = HandlerPid, parent_pid = ParentPid}}.
 
 handle_call(_Request, _From, State) ->
@@ -53,12 +53,32 @@ handle_info({processing_submit, SeqNum, MessageTag, List}, State) ->
             NewSubmitList = esmpp_utils:delete(SeqNum, State#state.submit_check),
             {noreply, State#state{submit_check = NewSubmitList}}
     end;
-handle_info({push_submit, Message}, State) ->
-    {noreply, State#state{submit_check = [Message | State#state.submit_check]}};
+handle_info({push_submit, {_SeqNumber, Ts} = Message}, State) ->
+
+    TimerRef = case State#state.timer_ref =:= undefined andalso State#state.submit_check =:= [] of
+        true ->
+            TriggerMs = next_timer_inteval(Ts, State#state.submit_timeout_ms),
+            ?LOG_INFO(<<"push_submit check for next non ack messages in ~p ms">>, [TriggerMs]),
+            erlang:send_after(TriggerMs, self(), check_not_ack_submit);
+        _ ->
+            undefined
+    end,
+
+    {noreply, State#state{submit_check = [Message | State#state.submit_check], timer_ref = TimerRef}};
 handle_info(check_not_ack_submit, State) ->
-    NewSubmitList = check_submit_expired(State),
-    erlang:send_after(?DEFAULT_CHECK_INTERVAL, self(), check_not_ack_submit),
-    {noreply, State#state{submit_check = NewSubmitList}};
+
+    {NewSubmitList, LowestTs} = check_submit_expired(State),
+
+    TimerRef = case LowestTs of
+        undefined ->
+            undefined;
+        _ ->
+            TriggerMs = next_timer_inteval(LowestTs, State#state.submit_timeout_ms),
+            ?LOG_INFO(<<"check for next non ack messages in ~p ms">>, [TriggerMs]),
+            erlang:send_after(TriggerMs, self(), check_not_ack_submit)
+    end,
+
+    {noreply, State#state{submit_check = NewSubmitList, timer_ref = TimerRef}};
 handle_info(Info, State) ->
     ?LOG_DEBUG("Unknown info msg ~p~n", [Info]),
     {noreply, State}.
@@ -72,19 +92,26 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 
 check_submit_expired(State) ->
-    check_submit_expired(State#state.submit_timeout_ms, esmpp_utils:now(), State#state.parent_pid, State#state.handler, State#state.submit_check, []).
+    check_submit_expired(State, esmpp_utils:now(), State#state.submit_check, [], undefined).
 
-check_submit_expired(_Timeout, _NowMs, _ParentPid,  _HandlerPid, [], Acc) ->
-    Acc;
-check_submit_expired(Timeout, NowMs, ParentPid, HandlerPid, [{SeqNum, MessageTs} = H|T], Acc) ->
-    Acc1 = case NowMs - MessageTs >= Timeout of
+check_submit_expired(_State, _NowMs, [], Acc, NewTs) ->
+    {Acc, NewTs};
+check_submit_expired(State, NowMs, [{SeqNum, MessageTs} = H|T], Acc, AccTs) ->
+    case NowMs - MessageTs >= State#state.submit_timeout_ms of
         true ->
-            esmpp_utils:send_notification(HandlerPid, {submit_error, ParentPid, SeqNum}),
-            Acc;
+            esmpp_utils:send_notification(State#state.handler, {submit_error, State#state.parent_pid, SeqNum}),
+            check_submit_expired(State, NowMs, T, Acc, AccTs);
         _ ->
-            [H|Acc]
-    end,
-    check_submit_expired(Timeout, NowMs, ParentPid, HandlerPid, T, Acc1).
+            check_submit_expired(State, NowMs, T, [H|Acc], get_new_timestamp(AccTs, MessageTs))
+    end.
+
+next_timer_inteval(NextMsgExpireTs, SubmitTimeoutMs) ->
+    erlang:max(?MIN_EXPIRE_ACCURACY_MS, (NextMsgExpireTs + SubmitTimeoutMs) - esmpp_utils:now()).
+    
+get_new_timestamp(undefined, NewValue) ->
+    NewValue;
+get_new_timestamp(CurrentValue, NewValue) ->
+    erlang:min(CurrentValue, NewValue).
 
 get_timeout(Key, Param) ->
     case esmpp_utils:lookup(Key, Param) of
